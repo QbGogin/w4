9c36b11b678248f9a69185973facfa624bd10c5f
QbGogin <egor.dashko.00@mail.ru>
2020-04-09 10:37:11 +0300

    third

--- index.php
+++ index.php
@@ -1,100 +1,208 @@
-<!doctype html>
-<html lang="ru">
+<?php
 
- <head>
-    <meta charset="UTF-8">
-    <link href="style.css" rel="stylesheet">    
- </head>
+header('Content-Type: text/html; charset=UTF-8');
 
- <body>
-   <div class="all" id="form">
-      <h2> Задания , связанные с формой </h2>
-      <?php
-      if (!empty($messages)) {
-        print('<div id="messages">');
-        foreach ($messages as $mess) {
-      	print($mess);
-        }
-        print('</div><br/><br/>');
-      }
-      ?>
-      <form target="_blank" action="myPHP.php" method="POST">
-          Имя: 
-          <br>
-          <input type="text" name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>">
-        <br>
-          E-mail:
-          <br>
-          <input type="email" name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>">
-        <br>
-          Дата рождения:
-          <br>
-          <input name="date" <?php if ($errors['date']) {print 'class="error"';} ?> value="<?php print $values['date']; ?>" type="text"/>
-        <br>
-        Пол:
-        <br>
-            <input type="radio" name="sex" value="Мужской" <?php if ($values['gender'] == 'Мужской') {print 'checked="checked"';} ?>/>
-            Мужской
-            <input type="radio" name="sex" value="Женский" <?php if ($values['gender'] == 'Женский') {print 'checked="checked"';} ?>/>
-            Женский
-            <input type="radio" name="sex" value="Ламинат" <?php if ($values['gender'] == 'Ламинат') {print 'checked="checked"';} ?>/>
-            Ламинат
+if ($_SERVER['REQUEST_METHOD'] == 'GET') {
+	$messages = array();
 
-            <input type="radio" name="sex" value="Линолеум" <?php if ($values['gender'] == 'Линолеум') {print 'checked="checked"';} ?>/>Женский<br/>
-                Линолеум
-        <br>
-        Количество глаз:
-        <br>
-            <input type="radio" name="limb" value="1" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '1') {print 'checked="checked"';} ?> />
-            1
-            <input type="radio" name="limb" value="2" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '2') {print 'checked="checked"';} ?> />
-            2
-            <input type="radio" name="limb" value="3" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '3') {print 'checked="checked"';} ?> />
-            3
-            <input type="radio" name="limb" value="4" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '4') {print 'checked="checked"';} ?> />
-            4
-            <input type="radio" name="limb" value="6" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '6') {print 'checked="checked"';} ?> />
-            6
-            <input type="radio" name="limb" value="8" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '8') {print 'checked="checked"';} ?> />
-            8
-        <br>
-        Сверхспособности:
-        <br>
-            <input type="checkbox" name="ability1" value="Непробиваемость" <?php if ($values['ability1'] != '') {print 'checked="checked"';} ?> />
-            Непробиваемость
-        <br>
-            <input type="checkbox" name="ability2" value="Супер-скорость" <?php if ($values['ability2'] != '') {print 'checked="checked"';} ?> />        
-             Супер-скорость
-        <br>
-            <input type="checkbox" name="ability3" value="Левитация" <?php if ($values['ability3'] != '') {print 'checked="checked"';} ?> />        
-             Левитация
-        <br>
-            <input type="checkbox" name="ability4" value="Невидимость" <?php if ($values['ability4'] != '') {print 'checked="checked"';} ?> />        
-             Невидимость
-        <br>
-            <input type="checkbox" name="ability5" value="Способность видеть сквозь стены" <?php if ($values['ability5'] != '') {print 'checked="checked"';} ?> />        
-             Способность видеть сквозь стены
-        <br>
-            <input type="checkbox" name="ability6" value="Бессмертие" <?php if ($values['ability6'] != '') {print 'checked="checked"';} ?> />        
-             Бессмертие
-        <br>
-            <input type="checkbox" name="ability7" value="Регенерация" <?php if ($values['ability7'] != '') {print 'checked="checked"';} ?> />        
-             Регенерация
-        <br>
-            <input type="checkbox" name="ability8" value="Деньги" <?php if ($values['ability8'] != '') {print 'checked="checked"';} ?> />        
-             Деньги
-        <br>
-          Биография:
-          <br>
-          <textarea name="osebe" value="<?php print $values['message']; ?>">Расскажите о себе</textarea>
-          <br>
-        <br>kontract
-            <input type="checkbox" name="kontract" value="+" <?php if ($values['kontract'] != '') {print 'checked="checked"';} ?>/>С контрактом ознакомлен<br/>
-            С контрактом ознакомлен
+	if (!empty($_COOKIE['save'])) { 
+		setcookie('save', '', 100000);
+		$messages[] = 'Спасибо, результаты сохранены.';
+	}
 
-          <br>
-        <input type="submit" name="go" value="Отправить">
-      </form>
-      </div>
-   </body>
-</html>
+	$errors = array();
+    $errors['name'] = !empty($_COOKIE['name_error']);
+    $errors['email'] = !empty($_COOKIE['email_error']);
+    $errors['date'] = !empty($_COOKIE['date_error']);
+    $errors['sex'] = !empty($_COOKIE['sex_error']);
+    $errors['limb'] = !empty($_COOKIE['limb_error']);
+    $errors['ability'] = !empty($_COOKIE['ability_error']);
+    $errors['kontract'] = !empty($_COOKIE['kontract_error']);
+
+	if ($errors['name']) {
+		setcookie('name_error', '', 100000);
+		$messages[] = '<div class="error">Укажите имя.</div>';
+	  }
+	  
+	  if ($errors['email']) {
+		setcookie('email_error', '', 100000);
+		$messages[] = '<div class="error">Адрес эл.почты указан неверно. Образец: exp@mail.ru</div>';
+	  }
+	 
+	  if ($errors['date']) {
+		setcookie('date_error', '', 100000);
+		$messages[] = '<div class="error">Дата рождения указана неверно. Образец: ДД.ММ.ГГГГ</div>';
+	  }
+	  
+	  if ($errors['sex']) {
+		setcookie('sex_error', '', 100000);
+		$messages[] = '<div class="error">Укажите пол.</div>';
+	  }
+	
+	  if ($errors['limb']) {
+		setcookie('limb_error', '', 100000);
+		$messages[] = '<div class="error">Укажите число конечностей.</div>';
+	  }
+	
+	  if ($errors['ability']) {
+		setcookie('ability_error', '', 100000);
+		$messages[] = '<div class="error">Укажите суперспособности.</div>';
+	  }
+	
+	  if ($errors['kontract']) {
+		setcookie('kontract_error', '', 100000);
+		$messages[] = '<div class="error">Пожалуйста, ознакомьтесь с контрактом!</div>';
+	  }
+
+	  $values = array();
+      $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
+      $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
+      $values['date'] = empty($_COOKIE['date_value']) ? '' : $_COOKIE['date_value'];
+      $values['sex'] = empty($_COOKIE['sex_value']) ? '' : $_COOKIE['sex_value'];
+      $values['limb'] = empty($_COOKIE['limb_value']) ? '' : $_COOKIE['limb_value'];
+      $values['ability1'] = empty($_COOKIE['ability1_value']) ? '' : $_COOKIE['ability1_value'];
+      $values['ability2'] = empty($_COOKIE['ability2_value']) ? '' : $_COOKIE['ability2_value'];
+      $values['ability3'] = empty($_COOKIE['ability3_value']) ? '' : $_COOKIE['ability3_value'];
+	  $values['ability4'] = empty($_COOKIE['ability4_value']) ? '' : $_COOKIE['ability4_value'];
+	  $values['ability5'] = empty($_COOKIE['ability5_value']) ? '' : $_COOKIE['ability5_value'];
+      $values['ability6'] = empty($_COOKIE['ability6_value']) ? '' : $_COOKIE['ability6_value'];
+      $values['ability7'] = empty($_COOKIE['ability7_value']) ? '' : $_COOKIE['ability7_value'];
+      $values['ability8'] = empty($_COOKIE['ability8_value']) ? '' : $_COOKIE['ability8_value'];
+      $values['osebe'] = empty($_COOKIE['osebe_value']) ? '' : $_COOKIE['osebe_value'];
+      $values['kontract'] = empty($_COOKIE['kontract_value']) ? '' : $_COOKIE['kontract_value'];
+
+	  include('myPHP.php');
+	}
+
+	else {
+		$errors = FALSE;
+		if (empty($_POST['name'])) {
+		  setcookie('name_error', '1', time() + 24 * 60 * 60);
+		  $errors = TRUE;
+		}
+		else {
+		  setcookie('name_value', $_POST['name'], time() + 365 * 30 * 24 * 60 * 60);
+		}
+	  
+		if (!preg_match("|^[-0-9a-z_\.]+@[-0-9a-z_^\.]+\.[a-z]{2,6}$|i", $_POST['email'])) {
+		  setcookie('email_error', '1', time() + 24 * 60 * 60);
+		  $errors = TRUE;
+		}
+		else {
+		  setcookie('email_value', $_POST['email'], time() + 365 * 30 * 24 * 60 * 60);
+		}
+	  
+		if (!preg_match('/^(\d{1,2})\.(\d{1,2})(?:\.(\d{4}))?$/', $_POST['date'])) {
+		  setcookie('date_error', '1', time() + 24 * 60 * 60);
+		  $errors = TRUE;
+		}
+		else {
+		  setcookie('date_value', $_POST['date'], time() + 365 * 30 * 24 * 60 * 60);
+		}
+	  
+		if (empty($_POST['sex'])) {
+		  setcookie('sex_error', '1', time() + 24 * 60 * 60);
+		  $errors = TRUE;
+		}
+		else {
+		  setcookie('sex_value', $_POST['sex'], time() + 365 * 30 * 24 * 60 * 60);
+		}
+	  
+		if (empty($_POST['limb'])) {
+		  setcookie('limb_error', '1', time() + 24 * 60 * 60);
+		  $errors = TRUE;
+		}
+		else {
+		  setcookie('limb_value', $_POST['limb'], time() + 365 * 30 * 24 * 60 * 60);
+		}
+	  
+		if (!isset($_POST['ability1']) 
+		&& !isset($_POST['ability2']) 
+		&& !isset($_POST['ability3']) 
+		&& !isset($_POST['ability4'])
+		&& !isset($_POST['ability5'])
+		&& !isset($_POST['ability6'])
+		&& !isset($_POST['ability7'])
+		&& !isset($_POST['ability8'])) {
+		  setcookie('ability_error', '1', time() + 24 * 60 * 60);
+		  $errors = TRUE;
+		}
+		else {
+		  setcookie('ability1_value', isset($_POST['ability1']) ? $_POST['ability1'] : '', time() + 365 * 30 * 24 * 60 * 60);
+		  setcookie('ability2_value', isset($_POST['ability2']) ? $_POST['ability2'] : '', time() + 365 * 30 * 24 * 60 * 60);
+		  setcookie('ability3_value', isset($_POST['ability3']) ? $_POST['ability3'] : '', time() + 365 * 30 * 24 * 60 * 60);
+		  setcookie('ability4_value', isset($_POST['ability4']) ? $_POST['ability4'] : '', time() + 365 * 30 * 24 * 60 * 60);
+		  setcookie('ability5_value', isset($_POST['ability5']) ? $_POST['ability5'] : '', time() + 365 * 30 * 24 * 60 * 60);
+		  setcookie('ability6_value', isset($_POST['ability6']) ? $_POST['ability6'] : '', time() + 365 * 30 * 24 * 60 * 60);
+		  setcookie('ability7_value', isset($_POST['ability7']) ? $_POST['ability7'] : '', time() + 365 * 30 * 24 * 60 * 60);
+		  setcookie('ability8_value', isset($_POST['ability8']) ? $_POST['ability8'] : '', time() + 365 * 30 * 24 * 60 * 60);
+		}
+	  
+		if (!empty($_POST['osebe'])) {
+		  setcookie('osebe_value', $_POST['osebe'], time() + 365 * 30 * 24 * 60 * 60);
+		}
+	  
+		if (empty($_POST['kontract'])) {
+		  setcookie('kontract_error', '1', time() + 24 * 60 * 60);
+		  $errors = TRUE;
+		}
+		else {
+		  setcookie('kontract_value', $_POST['kontract'], time() + 365 * 30 * 24 * 60 * 60);
+		}
+	  
+		if ($errors) {
+		  header('Location: index.php');
+		  exit();
+		}
+		else {
+		  setcookie('name_error', '', 100000);
+		  setcookie('email_error', '', 100000);
+		  setcookie('date_error', '', 100000);
+		  setcookie('sex_error', '', 100000);
+		  setcookie('limb_error', '', 100000);
+		  setcookie('ability_error', '', 100000);
+		  setcookie('kontract_error', '', 100000);
+		
+			$name = $_POST['name'];
+		    $email = $_POST['email'];
+		    $date = $_POST['date'];
+		    $sex = $_POST['sex'];
+		    $limb = $_POST['limb'];
+		    $ability = $_POST['ability1'].
+		      (isset($_POST['ability2']) ? (' '. $_POST['ability2']) : '').
+		      (isset($_POST['ability3']) ? (' '. $_POST['ability3']) : '').
+			  (isset($_POST['ability4']) ? (' '. $_POST['ability4']) : '');
+			  (isset($_POST['ability5']) ? (' '. $_POST['ability5']) : '');
+			  (isset($_POST['ability6']) ? (' '. $_POST['ability6']) : '');
+			  (isset($_POST['ability7']) ? (' '. $_POST['ability7']) : '');
+			  (isset($_POST['ability8']) ? (' '. $_POST['ability8']) : '');
+		    $osebe = $_POST['osebe'];
+		
+		
+		
+			$login = 'u20294';
+			$password = '5205554';
+			$data_base = new PDO('mysql:host=localhost;dbname=u20294', $login, $password);
+		
+			try {
+				$query = $data_base->prepare("INSERT INTO profi (name, email, date, sex, limb, ability, osebe) VALUES (:name, :email, :date, :sex, :limb, :ability, :osebe)");
+				$query->bindParam(':name', $name);
+				$query->bindParam(':email', $email);
+				$query->bindParam(':date', $date);
+				$query->bindParam(':sex', $sex);
+				$query->bindParam(':limb', $limb);
+				$query->bindParam(':ability',$ability);
+				$query->bindParam(':osebe', $osebe);
+				$query->execute();
+				print('Информация заполнена правильно. Приходите еще :)');
+				exit();
+			}
+			catch(PDOException $t){
+				print('Error : ' . $t->getosebe());
+				exit();
+			}
+		}
+		setcookie('save', '1');
+
+		header('Location: index.php');
+	}
--- myPHP.php
+++ myPHP.php
@@ -1,221 +1,143 @@
-<?php
+<!doctype html>
+<html lang="ru">
 
-header('Content-Type: text/html; charset=UTF-8');
+ <head>
+    <meta charset="UTF-8">
+    <link href="style.css" rel="stylesheet">    
+ </head>
 
-if ($_SERVER['REQUEST_METHOD'] == 'GET') {
-	$osebes = array();
-
-	if (!empty($_COOKIE['save'])) { 
-		setcookie('save', '', 100000);
-		$osebes[] = 'Спасибо, результаты сохранены.';
-	}
-
-	$errors = array();
-    $errors['name'] = !empty($_COOKIE['name_error']);
-    $errors['email'] = !empty($_COOKIE['email_error']);
-    $errors['date'] = !empty($_COOKIE['date_error']);
-    $errors['sex'] = !empty($_COOKIE['sex_error']);
-    $errors['limb'] = !empty($_COOKIE['limb_error']);
-    $errors['abi$ability'] = !empty($_COOKIE['abi$ability_error']);
-    $errors['kontract'] = !empty($_COOKIE['kontract_error']);
-
-	if ($errors['name']) {
-		setcookie('name_error', '', 100000);
-		$osebes[] = '<div class="error">Укажите имя.</div>';
-	  }
-	  
-	  if ($errors['email']) {
-		setcookie('email_error', '', 100000);
-		$osebes[] = '<div class="error">Адрес эл.почты указан неверно. Образец: exp@mail.ru</div>';
-	  }
-	 
-	  if ($errors['date']) {
-		setcookie('date_error', '', 100000);
-		$osebes[] = '<div class="error">Дата рождения указана неверно. Образец: ДД.ММ.ГГГГ</div>';
-	  }
-	  
-	  if ($errors['sex']) {
-		setcookie('sex_error', '', 100000);
-		$osebes[] = '<div class="error">Укажите пол.</div>';
-	  }
-	
-	  if ($errors['limb']) {
-		setcookie('limb_error', '', 100000);
-		$osebes[] = '<div class="error">Укажите число конечностей.</div>';
-	  }
-	
-	  if ($errors['abi$ability']) {
-		setcookie('abi$ability_error', '', 100000);
-		$osebes[] = '<div class="error">Укажите суперспособности.</div>';
-	  }
-	
-	  if ($errors['kontract']) {
-		setcookie('kontract_error', '', 100000);
-		$osebes[] = '<div class="error">Пожалуйста, ознакомьтесь с контрактом!</div>';
-	  }
-
-	  $values = array();
-      $values['name'] = empty($_COOKIE['name_value']) ? '' : $_COOKIE['name_value'];
-      $values['email'] = empty($_COOKIE['email_value']) ? '' : $_COOKIE['email_value'];
-      $values['date'] = empty($_COOKIE['date_value']) ? '' : $_COOKIE['date_value'];
-      $values['sex'] = empty($_COOKIE['sex_value']) ? '' : $_COOKIE['sex_value'];
-      $values['limb'] = empty($_COOKIE['limb_value']) ? '' : $_COOKIE['limb_value'];
-      $values['ability1'] = empty($_COOKIE['ability1_value']) ? '' : $_COOKIE['ability1_value'];
-      $values['ability2'] = empty($_COOKIE['ability2_value']) ? '' : $_COOKIE['ability2_value'];
-      $values['ability3'] = empty($_COOKIE['ability3_value']) ? '' : $_COOKIE['ability3_value'];
-	  $values['ability4'] = empty($_COOKIE['ability4_value']) ? '' : $_COOKIE['ability4_value'];
-	  $values['ability1'] = empty($_COOKIE['ability5_value']) ? '' : $_COOKIE['ability5_value'];
-      $values['ability2'] = empty($_COOKIE['ability6_value']) ? '' : $_COOKIE['ability6_value'];
-      $values['ability3'] = empty($_COOKIE['ability7_value']) ? '' : $_COOKIE['ability7_value'];
-      $values['ability4'] = empty($_COOKIE['ability8_value']) ? '' : $_COOKIE['ability8_value'];
-      $values['osebe'] = empty($_COOKIE['osebe_value']) ? '' : $_COOKIE['osebe_value'];
-      $values['kontract'] = empty($_COOKIE['kontract_value']) ? '' : $_COOKIE['kontract_value'];
-
-	  include('index.php');
-
-	  else {
-		$errors = FALSE;
-		if (empty($_POST['name'])) {
-		  setcookie('name_error', '1', time() + 24 * 60 * 60);
-		  $errors = TRUE;
-		}
-		else {
-		  setcookie('name_value', $_POST['name'], time() + 365 * 30 * 24 * 60 * 60);
-		}
-	  
-		if (!preg_match("|^[-0-9a-z_\.]+@[-0-9a-z_^\.]+\.[a-z]{2,6}$|i", $_POST['email'])) {
-		  setcookie('email_error', '1', time() + 24 * 60 * 60);
-		  $errors = TRUE;
-		}
-		else {
-		  setcookie('email_value', $_POST['email'], time() + 365 * 30 * 24 * 60 * 60);
-		}
-	  
-		if (!preg_match('/^(\d{1,2})\.(\d{1,2})(?:\.(\d{4}))?$/', $_POST['date'])) {
-		  setcookie('date_error', '1', time() + 24 * 60 * 60);
-		  $errors = TRUE;
-		}
-		else {
-		  setcookie('date_value', $_POST['date'], time() + 365 * 30 * 24 * 60 * 60);
-		}
-	  
-		if (empty($_POST['sex'])) {
-		  setcookie('sex_error', '1', time() + 24 * 60 * 60);
-		  $errors = TRUE;
-		}
-		else {
-		  setcookie('sex_value', $_POST['sex'], time() + 365 * 30 * 24 * 60 * 60);
-		}
-	  
-		if (empty($_POST['limb'])) {
-		  setcookie('limb_error', '1', time() + 24 * 60 * 60);
-		  $errors = TRUE;
-		}
-		else {
-		  setcookie('limb_value', $_POST['limb'], time() + 365 * 30 * 24 * 60 * 60);
-		}
-	  
-		if (!isset($_POST['ability1']) 
-		&& !isset($_POST['ability2']) 
-		&& !isset($_POST['ability3']) 
-		&& !isset($_POST['ability4'])
-		&& !isset($_POST['ability5'])
-		&& !isset($_POST['ability6'])
-		&& !isset($_POST['ability7'])
-		&& !isset($_POST['ability8'])) {
-		  setcookie('ability_error', '1', time() + 24 * 60 * 60);
-		  $errors = TRUE;
-		}
-		else {
-		  setcookie('ability1_value', isset($_POST['ability1']) ? $_POST['ability1'] : '', time() + 365 * 30 * 24 * 60 * 60);
-		  setcookie('ability2_value', isset($_POST['ability2']) ? $_POST['ability2'] : '', time() + 365 * 30 * 24 * 60 * 60);
-		  setcookie('ability3_value', isset($_POST['ability3']) ? $_POST['ability3'] : '', time() + 365 * 30 * 24 * 60 * 60);
-		  setcookie('ability4_value', isset($_POST['ability4']) ? $_POST['ability4'] : '', time() + 365 * 30 * 24 * 60 * 60);
-		  setcookie('ability5_value', isset($_POST['ability5']) ? $_POST['ability5'] : '', time() + 365 * 30 * 24 * 60 * 60);
-		  setcookie('ability6_value', isset($_POST['ability6']) ? $_POST['ability6'] : '', time() + 365 * 30 * 24 * 60 * 60);
-		  setcookie('ability7_value', isset($_POST['ability7']) ? $_POST['ability7'] : '', time() + 365 * 30 * 24 * 60 * 60);
-		  setcookie('ability8_value', isset($_POST['ability8']) ? $_POST['ability8'] : '', time() + 365 * 30 * 24 * 60 * 60);
-		}
-	  
-		if (!empty($_POST['osebe'])) {
-		  setcookie('osebe_value', $_POST['osebe'], time() + 365 * 30 * 24 * 60 * 60);
-		}
-	  
-		if (empty($_POST['kontract'])) {
-		  setcookie('kontract_error', '1', time() + 24 * 60 * 60);
-		  $errors = TRUE;
-		}
-		else {
-		  setcookie('kontract_value', $_POST['kontract'], time() + 365 * 30 * 24 * 60 * 60);
-		}
-	  
-		if ($errors) {
-		  header('Location: index.php');
-		  exit();
-		}
-		else {
-		  setcookie('name_error', '', 100000);
-		  setcookie('email_error', '', 100000);
-		  setcookie('date_error', '', 100000);
-		  setcookie('sex_error', '', 100000);
-		  setcookie('limb_error', '', 100000);
-		  setcookie('ability_error', '', 100000);
-		  setcookie('kontract_error', '', 100000);
-		  
-			
-if($_POST['go'] == 'Отправить'){
-	if ($_POST['kontract'] == ''){
-		print('Вы не ознакомились с контрактом. Пожалуйста, ознакомьтесь с ним!');
-		exit();
-	}
-	
-    foreach($_POST as $key => $val){
-        if(empty($val)){
-			print('Вы что-то не ввели. Пожалуйста, дополните свою анкету! ');
-			exit();
+ <body>
+   <div class="all" id="form">
+      <h2> Задания , связанные с формой </h2>
+      <?php
+      if (!empty($messages)) {
+        print('<div id="messages">');
+        foreach ($messages as $mess) {
+      	print($mess);
         }
-    }
-    
-	$name = $_POST['name'];
-    $email = $_POST['email'];
-    $date = $_POST['date'];
-    $sex = $_POST['sex'];
-    $limb = $_POST['limb'];
-    $ability = $_POST['ability1'].
-      (isset($_POST['ability2']) ? (' '. $_POST['ability2']) : '').
-      (isset($_POST['ability3']) ? (' '. $_POST['ability3']) : '').
-	  (isset($_POST['ability4']) ? (' '. $_POST['ability4']) : '');
-	  (isset($_POST['ability5']) ? (' '. $_POST['ability5']) : '');
-	  (isset($_POST['ability6']) ? (' '. $_POST['ability6']) : '');
-	  (isset($_POST['ability7']) ? (' '. $_POST['ability7']) : '');
-	  (isset($_POST['ability8']) ? (' '. $_POST['ability8']) : '');
-    $osebe = $_POST['osebe'];
-
-
-
-	$login = 'u20294';
-	$password = '5205554';
-	$data_base = new PDO('mysql:host=localhost;dbname=u20294', $login, $password);
-
-	try {
-		$query = $data_base->prepare("INSERT INTO profi (name, email, date, sex, limb, abi$ability, osebe) VALUES (:name, :email, :date, :sex, :limb, :abi$ability, :osebe)");
-		$query->bindParam(':name', $name);
-		$query->bindParam(':email', $email);
-		$query->bindParam(':date', $date);
-		$query->bindParam(':sex', $sex);
-		$query->bindParam(':limb', $limb);
-		$query->bindParam(':abi$ability',$ability);
-		$query->bindParam(':osebe', $osebe);
-		$query->execute();
-		print('Информация заполнена правильно. Приходите еще :)');
-		exit();
-	}
-	catch(PDOException $t){
-		print('Error : ' . $t->getosebe());
-		exit();
-	}
-}
-setcookie('save', '1');
-
-header('Location: index.php');
-?>
+        print('</div><br/><br/>');
+      }
+      ?>
+      <form action="" method="POST">
+        <label>
+          Имя: 
+          <br>
+          <input type="text" name="name" <?php if ($errors['name']) {print 'class="error"';} ?> value="<?php print $values['name']; ?>">
+        </label>
+        <br>
+        <label>
+          E-mail:
+          <br>
+          <input type="email" name="email" <?php if ($errors['email']) {print 'class="error"';} ?> value="<?php print $values['email']; ?>">
+        </label>
+        <br>
+        <label>
+          Дата рождения:
+          <br>
+          <input name="date" <?php if ($errors['date']) {print 'class="error"';} ?> value="<?php print $values['date']; ?>" type="text"/>
+        </label>
+        <br>
+        Пол:
+        <br>
+        <label>
+            <input type="radio" name="sex" value="male" <?php if ($values['sex'] == 'male') {print 'checked="checked"';} ?>/>
+            Мужской
+        </label>
+        <label>
+            <input type="radio" name="sex" value="female" <?php if ($values['sex'] == 'female') {print 'checked="checked"';} ?>/>
+            Женский
+        </label>
+        <label>
+            <input type="radio" name="sex" value="lamin" <?php if ($values['sex'] == 'lamin') {print 'checked="checked"';} ?>/>
+            Ламинат
+         </label>
+        <label>
+            <input type="radio" name="sex" value="linol" <?php if ($values['sex'] == 'linol') {print 'checked="checked"';} ?>/>
+                Линолеум
+        </label>
+        <br> 
+        Количество глаз:
+        <br>
+        <label>
+            <input type="radio" name="limb" value="1" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '1') {print 'checked="checked"';} ?> />
+            1
+        </label>
+        <label>
+            <input type="radio" name="limb" value="2" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '2') {print 'checked="checked"';} ?> />
+            2
+        </label>
+        <label>
+            <input type="radio" name="limb" value="3" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '3') {print 'checked="checked"';} ?> />
+            3
+        </label>
+        <label>
+            <input type="radio" name="limb" value="4" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '4') {print 'checked="checked"';} ?> />
+            4
+        </label>
+        <label>
+            <input type="radio" name="limb" value="6" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '6') {print 'checked="checked"';} ?> />
+            6
+        </label>
+        <label>
+            <input type="radio" name="limb" value="8" <?php if ($errors['limb']) {print 'class="error"';} ?>  <?php if ($values['limb'] == '8') {print 'checked="checked"';} ?> />
+            8
+        </label>
+        <br>
+        Сверхспособности:
+        <br>
+        <label>
+            <input type="checkbox" name="ability1" value="Непробиваемость" <?php if ($values['ability1'] != '') {print 'checked="checked"';} ?> />
+            Непробиваемость
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="ability2" value="Супер-скорость" <?php if ($values['ability2'] != '') {print 'checked="checked"';} ?> />        
+             Супер-скорость
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="ability3" value="Левитация" <?php if ($values['ability3'] != '') {print 'checked="checked"';} ?> />        
+             Левитация
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="ability4" value="Невидимость" <?php if ($values['ability4'] != '') {print 'checked="checked"';} ?> />        
+             Невидимость
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="ability5" value="Способность видеть сквозь стены" <?php if ($values['ability5'] != '') {print 'checked="checked"';} ?> />        
+             Способность видеть сквозь стены
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="ability6" value="Бессмертие" <?php if ($values['ability6'] != '') {print 'checked="checked"';} ?> />        
+             Бессмертие
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="ability7" value="Регенерация" <?php if ($values['ability7'] != '') {print 'checked="checked"';} ?> />        
+             Регенерация
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="ability8" value="Деньги" <?php if ($values['ability8'] != '') {print 'checked="checked"';} ?> />        
+             Деньги
+        </label>
+        <br>
+        <label>
+          Биография:
+          <br>
+          <textarea name="osebe" value="<?php print $values['message']; ?>">Расскажите о себе</textarea>
+          <br>
+        </label>
+        <br>
+        <label>
+            <input type="checkbox" name="kontract" value="+" <?php if ($values['kontract'] != '') {print 'checked="checked"';} ?>/>С контрактом ознакомлен<br/>
+          </label>
+          <br>
+        <input type="submit" name="go" value="Отправить">
+      </form>
+   </div>
+   </body>
+</html>
